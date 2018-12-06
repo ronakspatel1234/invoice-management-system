636b35cf313c1392182a6476b5cbc92c3632c1b6
Akshita <pandeyraj467@gmail.com>
2018-12-06 19:26:15 +0530

    feat: create structure add html and routing

---
+++ src/app/products/products.model.ts
@@ -0,0 +1,11 @@
+export class Products
+{
+    id:number;
+    product_number:string;
+    description:string;
+    uom:string;
+    price:number;
+    image:string;
+    date:string;
+    group:string;
+}
