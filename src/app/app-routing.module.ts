70ae681fb95d896fe61db52124fa4a242dd8c6c3
ronakspatel <ronakspatel1234@gmail.com>
2018-12-07 16:09:33 +0530

    feat: add invoice and merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
     loadChildren: './user-profile/user-profile.module#UserProfileModule'
   },
   {
+    path: 'invoice',
+    loadChildren: './invoices/invoices.module#InvoicesModule'
+  },
+  {
     path: 'login',
     component: LoginComponent
   },
