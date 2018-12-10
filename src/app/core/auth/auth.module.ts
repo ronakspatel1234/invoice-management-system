99a5182d8fc2aaebbb6826b2a4fe49090df2743c
ronakspatel <ronakspatel1234@gmail.com>
2018-12-10 14:39:12 +0530

    fix: scss js and add script

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -4,12 +4,14 @@
  */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
 // ----------------------------------------------------------------------------------------------//
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [LoginComponent]
 })
