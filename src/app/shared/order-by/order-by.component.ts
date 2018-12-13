2c4efe0646083c6e9808ab83a288846b6dea4104
ronakspatel <ronakspatel1234@gmail.com>
2018-12-13 14:57:45 +0530

    fix: customer error solve

--- src/app/shared/order-by/order-by.component.ts
+++ src/app/shared/order-by/order-by.component.ts
@@ -1,20 +1,19 @@
 /**
  * @author Vaibhavi Prajapati
  */
-import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
-import { OrderByKey } from "./order-by-keys.model";
-import { Sort } from "./sort.model";
-import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { OrderByKey } from './order-by-keys.model';
+import { Sort } from './sort.model';
+// import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: "ims-order-by",
-  templateUrl: "./order-by.component.html",
-  styleUrls: ["./order-by.component.scss"]
+  selector: 'ims-order-by',
+  templateUrl: './order-by.component.html',
+  styleUrls: ['./order-by.component.scss']
 })
 export class OrderByComponent implements OnInit {
   name: OrderByKey;
   public toggle = true;
-  dateForm: FormGroup;
   /**output for export button */
   @Output() exportData = new EventEmitter<string>();
 
@@ -30,36 +29,34 @@ export class OrderByComponent implements OnInit {
     return this.name;
   }
 
-  constructor(private fb: FormBuilder) {}
+  constructor() { }
 
   ngOnInit() {
-    this.dateForm = this.fb.group({
-      date: ["", [Validators.required]]
-    });
+
   }
   /**When user click on export button its give output to the parent */
   clickExport() {
     this.exportData.emit();
-    console.log("clicked");
+    console.log('clicked');
   }
   /**when user select the field its change with selected field */
   sortData(sort: Sort[]) {
     this.sortValue.emit(sort);
-    console.log("sort");
+    console.log('sort');
   }
 
-  public onSubmit(data) {
-    let dateFormatChange =
-      data.date.day +
-    "/" +
-    data.date.month +
-    "/" +
-    data.date.year;
-    console.log(data);
+  // public onSubmit(data) {
+  //   let dateFormatChange =
+  //     data.date.day +
+  //   '/' +
+  //   data.date.month +
+  //   '/' +
+  //   data.date.year;
+  //   console.log(data);
 
-    data.date= dateFormatChange;
-     console.log(data.date);
-  }
+  //   data.date= dateFormatChange;
+  //    console.log(data.date);
+  // }
   /** method for toggle up and down arrow
    * Its value can be true or false
    */
