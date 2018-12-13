4f0cd643322a278e481820c994c41909e62cdc6c
unknown <pvaibhavi.104@gmail.com>
2018-12-13 15:42:23 +0530

    fix: merge solved

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -9,6 +9,7 @@ import { SharedModule } from './../shared/shared.module';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { DetailComponent } from './detail/detail.component';
 import { AddComponent } from './add/add.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
